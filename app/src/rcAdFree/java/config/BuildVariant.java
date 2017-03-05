1edd1380887b0878f501bb955cdf3ff54d8412c7
tateisu <tateisu@gmail.com>
2017-03-06 04:15:51 +0900

    in-app-billing とか画面の小さい端末での表示の改善とか

---
+++ app/src/rcAdFree/java/config/BuildVariant.java
@@ -0,0 +1,8 @@
+package config;
+
+public class BuildVariant{
+	public static final String DATA_PROVIDER_AUTHORITY="jp.juggler.fadownloader.data";
+
+	public static final boolean IS_ADFREE = true;
+}
+
